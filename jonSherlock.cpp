4ad877e7e42e434116573d4418bb14b161101b21
rutayanp <rutayanp@gmail.com>
2014-02-15 15:12:19 -0800

    array questions

---
+++ jonSherlock.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int *arr =(int *)malloc(sizeof(int) * N);
+    int *new_start = arr;
+
+    if ( k > 0)
+        new_start  = &arr[N - k];
+
+    for ()
+}
